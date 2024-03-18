cfbedd31a4a19b49a62ca14aa51e639f54316e0b
Alvin-igobwa <53784550+Alvin-igobwa@users.noreply.github.com>
2024-03-18 13:22:08 +0300

    removing matplotlib

--- Backdating.py
+++ Backdating.py
@@ -5,9 +5,6 @@ import numpy as np
 # loading the library for the visualisation
 import streamlit as st
 
-# Visualisations
-import matplotlib.pyplot as plt
-
 # Correlation test
 from scipy import stats
 from scipy.stats import pearsonr
